f46a44f1b6e550b15c3cf210588060bc5c531f5d
dunas26 <jhonjamesillo@gmail.com>
2022-11-12 19:22:02 -0500

    UPDATE: Header setup

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,10 +1,43 @@
+import { ReactElement } from 'react';
+import { CgSpinner } from 'react-icons/cg';
+
 import styles from './Button.module.css';
 
 export interface ButtonProps {
 	label?: string;
 	click?: () => void;
+	icon?: ReactElement;
+	iconPlacement?: "left" | "right";
+	waiting?: boolean;
 }
 
-export function Button({ label = "", click = () => { } }: ButtonProps) {
-	return <button className={styles.buttonFrame} onClick={click}>{label}</button>
+export function Button({ label = "", click = () => { }, icon = undefined, iconPlacement = "left", waiting = false }: ButtonProps) {
+
+	function handleClick() {
+		if (waiting) return;
+		click();
+	}
+
+	function buttonContents() {
+		return <>
+			{!!icon && iconPlacement == "left" && icon}
+			{label}
+			{!!icon && iconPlacement == "right" && icon}
+		</>
+	}
+
+	function waitContents() {
+		return <div className={styles.waitFrame}>
+			<p className={styles.waitLabel}>{label}</p>
+			<CgSpinner className={styles.spinner} />
+		</div>
+	}
+
+	return <button className={styles.buttonFrame} onClick={handleClick}>
+		{
+			waiting
+				? waitContents()
+				: buttonContents()
+		}
+	</button>
 }
--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -1,15 +1,28 @@
-import { AiOutlineFlag } from "react-icons/ai";
+import { useEffect, useState } from "react";
+import { AiOutlineFlag, AiOutlineLineChart } from "react-icons/ai";
 
 import { Button, Title } from "@components/ui";
 import styles from "./Header.module.css";
 
 export function Header() {
+
+	const [waiting, setWaiting] = useState(false);
+
+	function temporaryWaiting() {
+		setWaiting(true);
+		setTimeout(() => setWaiting(false), 1000);
+	}
+
+	useEffect(() => {
+		temporaryWaiting()
+	}, [])
+
 	return <header className={styles.headerContainer}>
-		<section>
-			<Title icon={<AiOutlineFlag className="w-12 h-auto" />} title="Minesweeper" subtitle="Let's play" />
+		<section onClick={temporaryWaiting}>
+			<Title icon={<AiOutlineFlag className="w-12 h-auto text-amber-600" />} title="Minesweeper" subtitle="Let's play" />
 		</section>
 		<section>
-			<Button label="See my statistics" />
+			<Button label="See my statistics" icon={<AiOutlineLineChart className="w-6 h-auto text-amber-600" />} waiting={waiting} />
 		</section>
 	</header>
 }
