a4c16e6392c87ea2c349f9fbd9c03047a9217214
dunas26 <jhonjamesillo@gmail.com>
2022-11-13 15:01:27 -0500

    ADD: Navigation capabilities for main sidebar

--- src/layouts/Main/Main.tsx
+++ src/layouts/Main/Main.tsx
@@ -1,6 +1,7 @@
 import { ReactElement } from "react";
 import { RiGamepadLine } from "react-icons/ri";
 import { AiOutlineCalendar, AiOutlineTrophy } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
 
 import { Footer, Header, Sidebar } from "@layouts";
 import { SidebarButton } from "@components/ui";
@@ -13,11 +14,13 @@ export interface MainLayoutProps {
 
 export function Main({ children = undefined }: MainLayoutProps) {
 
+	const navigate = useNavigate();
+
 	function sidebarButtons(expanded: boolean) {
 		return <>
-			<SidebarButton label="Play the game" expanded={expanded} icon={<RiGamepadLine className="w-8 h-auto" />} />
-			<SidebarButton label="Daily challenge" expanded={expanded} icon={<AiOutlineCalendar className="w-8 h-auto" />} />
-			<SidebarButton label="Ranking" expanded={expanded} icon={<AiOutlineTrophy className="w-8 h-auto" />} />
+			<SidebarButton label="Play the game" click={() => navigate("/")} expanded={expanded} icon={<RiGamepadLine className="w-8 h-auto" />} />
+			<SidebarButton label="Daily challenge" click={() => navigate("/daily")} expanded={expanded} icon={<AiOutlineCalendar className="w-8 h-auto" />} />
+			<SidebarButton label="Ranking" click={() => navigate("/ranking")} expanded={expanded} icon={<AiOutlineTrophy className="w-8 h-auto" />} />
 		</>
 	}
 
