8bc16ae77531bcfa65209388f83a65db5feaa5a4
dunas26 <jhonjamesillo@gmail.com>
2022-11-14 13:56:31 -0500

    UPDATE: new placeholder to label display

--- src/components/ui/LabelDisplay/LabelDisplay.tsx
+++ src/components/ui/LabelDisplay/LabelDisplay.tsx
@@ -1,3 +1,4 @@
+import MessageService from "@services/Message.service";
 import styles from "./LabelDisplay.module.css";
 
 export interface LabelDisplayProps {
@@ -8,6 +9,6 @@ export interface LabelDisplayProps {
 export function LabelDisplay({ label, value }: LabelDisplayProps) {
 	return <article className={styles.labelContainer}>
 		<p className={styles.label}>{label}:</p>
-		<p className={styles.value}>{value}</p>
+		{!!value ? <p className={styles.value}>{value}</p> : <p className={styles.placeholder}>{MessageService.getMessage("no_value")}</p>}
 	</article>
 }
--- src/services/Message.service.ts
+++ src/services/Message.service.ts
@@ -2,6 +2,7 @@
 const messages: { [key: string]: string } = {
 	game_gameover: "Wish you luck next time.",
 	game_preparing: "Please wait while we prepare your board.",
+	no_value: "No value provided."
 }
 
 function getMessage(key: string): string {
