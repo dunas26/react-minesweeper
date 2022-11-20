bb6a79e515d2169e9bf8634f6148ef41ab3688e6
dunas26 <jhonjamesillo@gmail.com>
2022-11-20 15:07:33 -0500

    ADD: Footer social icons

--- src/layouts/Footer/Footer.tsx
+++ src/layouts/Footer/Footer.tsx
@@ -1,7 +1,12 @@
+import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
 import styles from "./Footer.module.css";
 
 export function Footer() {
 	return <footer className={styles.footerContainer}>
 		<p>Copyright &copy; John James Valencia - 2022</p>
+		<section className={styles.iconSection}>
+			<a href="https://linkedin.com/in/johnjamesvalencia" target="blank">{<AiFillLinkedin fontSize={21} />}</a>
+			<a href="https://github.com/dunas26" target="blank">{<AiFillGithub fontSize={21} />}</a>
+		</section>
 	</footer>
 }
